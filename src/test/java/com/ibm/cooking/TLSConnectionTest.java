f013565ba85be702a9d020cb962bdd48dc733ff8
machozhao <machozhao>
2017-03-17 10:01:20 +0800

    Init

--- src/test/java/com/ibm/cooking/TLSConnectionTest.java
+++ src/test/java/com/ibm/cooking/TLSConnectionTest.java
@@ -13,78 +13,78 @@ import java.util.Hashtable;
  * Created by zhaodonglu on 2017/3/16.
  */
 public class TLSConnectionTest {
-    @Test
-    public void testTCP() throws Exception {
-        // Set up environment for creating initial context
-        Hashtable env = new Hashtable(11);
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+  @Test
+  public void testTcp() throws Exception {
+    // Set up environment for creating initial context
+    Hashtable env = new Hashtable(11);
+    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
 
-        // Must use the name of the server that is found in its certificate
-        env.put(Context.PROVIDER_URL, "ldap://192.168.142.129:389");
+    // Must use the name of the server that is found in its certificate
+    env.put(Context.PROVIDER_URL, "ldap://192.168.142.129:389");
 
-        // Create initial context
-        LdapContext ctx = new InitialLdapContext(env, null);
+    // Create initial context
+    LdapContext ctx = new InitialLdapContext(env, null);
 
-        // Perform read
-        System.out.println(ctx.getAttributes("dc=com"));
+    // Perform read
+    System.out.println(ctx.getAttributes("dc=com"));
 
-        // Close the context when we're done
-        ctx.close();
+    // Close the context when we're done
+    ctx.close();
 
-    }
+  }
 
-    @Test
-    public void testSSL() throws Exception {
-        // Set up environment for creating initial context
-        Hashtable env = new Hashtable(11);
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+  @Test
+  public void testSSL() throws Exception {
+    // Set up environment for creating initial context
+    Hashtable env = new Hashtable(11);
+    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
 
-        // Must use the name of the server that is found in its certificate
-        env.put(Context.PROVIDER_URL, "ldaps://192.168.142.129:389");
+    // Must use the name of the server that is found in its certificate
+    env.put(Context.PROVIDER_URL, "ldaps://192.168.142.129:389");
 
-        // Create initial context
-        LdapContext ctx = new InitialLdapContext(env, null);
+    // Create initial context
+    LdapContext ctx = new InitialLdapContext(env, null);
 
-        // Perform read
-        System.out.println(ctx.getAttributes("dc=com"));
+    // Perform read
+    System.out.println(ctx.getAttributes("dc=com"));
 
-        // Close the context when we're done
-        ctx.close();
+    // Close the context when we're done
+    ctx.close();
 
-    }
+  }
 
-    @Test
-    public void testTLSNegotiateByDefaultJVMTrust() throws Exception {
-        // Set up environment for creating initial context
-        Hashtable env = new Hashtable(11);
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+  @Test
+  public void testTLSNegotiateByDefaultJVMTrust() throws Exception {
+    // Set up environment for creating initial context
+    Hashtable env = new Hashtable(11);
+    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
 
-        // Must use the name of the server that is found in its certificate
-        env.put(Context.PROVIDER_URL, "ldap://LDAP_Server:389");
+    // Must use the name of the server that is found in its certificate
+    env.put(Context.PROVIDER_URL, "ldap://LDAP_Server:389");
 
-        // Create initial context
-        LdapContext ctx = new InitialLdapContext(env, null);
+    // Create initial context
+    LdapContext ctx = new InitialLdapContext(env, null);
 
-        // Start TLS
-        StartTlsResponse tls = (StartTlsResponse)ctx.extendedOperation(new StartTlsRequest());
+    // Start TLS
+    StartTlsResponse tls = (StartTlsResponse) ctx.extendedOperation(new StartTlsRequest());
 
-        tls.negotiate();
+    // ... do something useful with ctx that requires secure connection
 
-        // ... do something useful with ctx that requires secure connection
+    tls.negotiate();
 
-        // Perform read
-        System.out.println(ctx.getAttributes("dc=COM"));
+    System.out.println(ctx.getAttributes("dc=COM"));
 
-        // Stop TLS
-        tls.close();
+    // Stop TLS
+    tls.close();
 
-        // ... do something useful with ctx that doesn't require security
+    // ... do something useful with ctx that doesn't require security
 
-        // Perform read
-        System.out.println(ctx.getAttributes("dc=com"));
+    // Perform read
+    System.out.println(ctx.getAttributes("dc=com"));
 
-        // Close the context when we're done
-        ctx.close();
+    // Close the context when we're done
+    ctx.close();
 
-    }
+  }
+
 }
